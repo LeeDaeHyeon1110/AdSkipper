13a5591b3f726fc71b34a28a3c955c69eac6e9fd
LeeDaeHyeon1110 <dleogus0910@gmail.com>
2022-11-12 12:01:51 +0900

    add press skip btn and ad close btn

--- content.js
+++ content.js
@@ -2,12 +2,17 @@ if (document.location.host == "www.youtube.com") {
     setInterval(() => {
         const video = document.querySelector("#movie_player > div.html5-video-container > video");
         if (video) {
+            // move ad duration bar to end
             const progressBar = document.querySelector("#movie_player > div.ytp-chrome-bottom > div.ytp-progress-bar-container > div.ytp-progress-bar > div.ytp-chapters-container > div > div.ytp-progress-list > div.ytp-play-progress.ytp-swatch-background-color");
             if (progressBar) {
                 if (window.getComputedStyle(progressBar).backgroundColor === "rgb(255, 204, 0)") {
                     video.currentTime = video.duration
                 }
             }
+            
+            document.getElementsByClassName("ytp-ad-skip-button ytp-button")[0]?.click() // click close button for ad skip
+            document.getElementsByClassName("ytp-ad-overlay-close-button")[0]?.click() // click close button for ad popup
+
         }
     }, 100);
 }
